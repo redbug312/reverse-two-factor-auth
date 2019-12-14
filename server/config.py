6c97ef248a94e3cfe59658735f69c218b13679d3
RedBug312 <redbug312@gmail.com>
2019-12-14 17:57:28 +0800

    Suppress FSADeprecationWarning message

--- server/config.py
+++ server/config.py
@@ -1,3 +1,5 @@
 SECRET_KEY = 'the quick brown fox jumps over the lazy dog'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
+
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
